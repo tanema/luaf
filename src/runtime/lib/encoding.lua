21d0e720f331aec8db478defc0c9d54cb5583fac
Tim Anema <tim.anema@pocket.health>
2025-10-28 13:15:30 -0400

    Use own json lib

--- src/runtime/lib/encoding.lua
+++ src/runtime/lib/encoding.lua
@@ -37,6 +37,7 @@ local function marshalLua(val)
 end
 
 local function unmarshalLua(str)
+	assert(type(str) == "string", "bad argument #1 to lua.unmarshal, expected string")
 	return load(str)()
 end
 
@@ -82,8 +83,106 @@ local function marshalJSON(val)
 	return dumpJSON(val, {})
 end
 
+local JSONDecoder = {}
+
+local function NewJSONDecoder(str)
+	return setmetatable({
+		buffer = str,
+		index = 1,
+	}, { __index = JSONDecoder })
+end
+
+function JSONDecoder:decode()
+	return self:decodeItem()
+end
+
+function JSONDecoder:decodeItem()
+	self:skipWhitespace()
+	local ch = string.sub(self.buffer, self.index, self.index)
+	if ch == '"' then
+		return self:parseString()
+	elseif ch == "[" then
+		return self:parseArray()
+	elseif ch == "{" then
+		return self:parseObject()
+	else
+		return self:parseLiteral()
+	end
+end
+
+function JSONDecoder:skipWhitespace()
+	while string.sub(self.buffer, self.index, self.index) == " " do
+		self.index = self.index + 1
+	end
+end
+
+function JSONDecoder:parseString()
+	self.index = self.index + 1
+	local start = self.index
+	while string.sub(self.buffer, self.index, self.index) ~= '"' do
+		self.index = self.index + 1
+	end
+	local str = string.sub(self.buffer, start, self.index - 1)
+	self.index = self.index + 1
+	return str
+end
+
+function JSONDecoder:parseLiteral()
+	local ch = string.sub(self.buffer, self.index, self.index)
+	local start = self.index
+	while ch ~= "," and ch ~= "}" and ch ~= "]" and ch ~= "" do
+		self.index = self.index + 1
+		ch = string.sub(self.buffer, self.index, self.index)
+	end
+	local literal = string.sub(self.buffer, start, self.index - 1)
+	if literal == "true" then
+		return true
+	elseif literal == "false" then
+		return false
+	elseif literal == "null" then
+		return nil
+	else
+		return tonumber(literal)
+	end
+end
+
+function JSONDecoder:parseArray()
+	local result = {}
+	self.index = self.index + 1 -- skip "["
+	while string.sub(self.buffer, self.index, self.index) ~= "]" do
+		table.insert(result, self:decodeItem())
+		if string.sub(self.buffer, self.index, self.index) == "," then
+			self.index = self.index + 1
+		end
+	end
+	self.index = self.index + 1 -- skip "]"
+	return result
+end
+
+function JSONDecoder:parseObject()
+	local result = {}
+	self.index = self.index + 1 -- skip "{"
+	while string.sub(self.buffer, self.index, self.index) ~= "}" do
+		local ch = string.sub(self.buffer, self.index, self.index)
+		assert(ch == '"', string.format('expected " to start object key but found %s at index %d', ch, self.index))
+		local key = self:parseString()
+
+		ch = string.sub(self.buffer, self.index, self.index)
+		assert(ch == ":", string.format("expected : but found %s at index %d", ch, self.index))
+		self.index = self.index + 1 -- skip ":"
+
+		result[key] = self:decodeItem()
+		if string.sub(self.buffer, self.index, self.index) == "," then
+			self.index = self.index + 1
+		end
+	end
+	self.index = self.index + 1 -- skip "}"
+	return result
+end
+
 local function unmarshalJSON(str)
-	error("not implemented")
+	assert(type(str) == "string", "bad argument #1 to json.unmarshal, expected string")
+	return NewJSONDecoder(str):decode()
 end
 
 return {
